3757f427e60c4ef1eb4d8277fb99d25bbdbb0447
harisha <harsha2002jc@gmail.com>
2026-08-23 02:16:02 +0530

    footer

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -2,7 +2,7 @@
 import AccessSection from "@/components/website/AccessSection/AccessSection";
 import GrowthStats from "@/components/website/GrowthStats/GrowthStats";
 import Hero from "@/components/website/Hero/Hero";
-import Job from "@/components/website/jobs/jobcard";
+
 import SkillsTrending from "@/components/website/SkillsTrending/SkillsTrending";
 import Testimonials from "@/components/website/Testimonials/Testimonials";
 import TrustedCompanies from "@/components/website/TrustedCompanies/TrustedCompanies";
@@ -24,7 +24,7 @@ export default function Home() {
       
       
       
-      <Job />
+      
       
     </main>
   );
